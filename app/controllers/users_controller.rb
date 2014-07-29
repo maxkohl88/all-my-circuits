8d5ce576298b662c2ef2a5fdaf6f777fe4d555d6
Jake Austin <jakeaust07@gmail.com>
2014-07-29 10:39:32 -0400

    handle 'both' preferences in index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,23 @@ class UsersController < ApplicationController
 
   def index
     # if no search terms, search gender + interested_in
-    # not properly handling 'both' --- might be better to represent as null
-    @users = User.where(gender: current_user.interested_in,
-                        interested_in: current_user.gender)
+    # Special cases for 'both'
+    if current_user.interested_in == 'both'
+      @users = User.where(interested_in: current_user.gender || 'both')
+    else
+      @users = User.where(gender: current_user.interested_in,
+                        interested_in: current_user.gender || 'both')
+    end
     # if search terms, compound search with each term's value
-    # browser accepts parameters, missing index.html.erb
     # looks like it will work if search options are submitted through params
     User::SEARCHABLE.each do |term|
       if params[term]
         @users = @users.select { |user| user[term] == params[term] }
       end
     end
+    binding.pry
   end
+
 
   def new
     #empty new route for github authentication
@@ -34,12 +39,6 @@ class UsersController < ApplicationController
 
   private
 
-  # def search_params
-  #   params.require(:user).permit(:bday, :zip, :gender, :interested_in, :height,
-  #                                :fav_animal, :game_genre, :music_genre,
-  #                                :movie_genre, :human_language)
-  # end
-
   def user_params
     params.require(:user).permit(:bday, :zip, :gender, :interested_in, :height,
                                  :fav_animal, :game_genre, :music_genre,
@@ -47,6 +46,5 @@ class UsersController < ApplicationController
                                  :fav_book, :industry, :years_programming,
                                  :primary_language)
   end
-
 
 end
