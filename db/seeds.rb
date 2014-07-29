b90c8eb3e2b60528dd749c023ab452219e301387
Jake Austin <jakeaust07@gmail.com>
2014-07-28 21:33:07 -0400

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
-10.times do |i|
+20.times do |i|
+  gender = 'female'
+  interest = 'male'
+  if i%2 == 0
+    interest = 'female'
+    gender = 'male'
+  end
+
   User.create!(email: Faker::Internet.safe_email, password: Faker::Internet.password,
                provider: 'github', uid: 10101010, name: "user #{i}", bday: '1990-01-01',
-               zip: 10024, gender: 'male', interested_in: 'female', height: 70, primary_language: 'c++',
-               human_language: 'English', years_programming: 2, industry: 'web',
+               zip: 10024, gender: gender, interested_in: interest, height: 70, primary_language: 'c++',
+               human_language: 'English', years_programming: i, industry: 'web',
                fav_animal: 'wombat', fav_book: 'catcher', game_genre: 'RPG', music_genre: 'rock',
                movie_genre: 'Horror', status: 'active', summary: 'Yoyo')
   puts "made user #{i}"
