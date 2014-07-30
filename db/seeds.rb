9f600dc62f9c39a18af3b1f04afa6fbb79c0a1c8
Jake Austin <jakeaust07@gmail.com>
2014-07-30 10:57:05 -0400

    change seeded users from c++ to c

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.delete_all
 
   User.create!(email: Faker::Internet.safe_email, password: Faker::Internet.password,
                provider: 'github', uid: 10101010, name: "user #{i}", bday: '1990-01-01',
-               zip: 10024, gender: gender, interested_in: interest, height: 60+i, primary_language: 'c++',
+               zip: 10024, gender: gender, interested_in: interest, height: 60+i, primary_language: 'c',
                human_language: 'English', years_programming: i, industry: 'web',
                fav_animal: 'wombat', fav_book: 'catcher', game_genre: 'RPG', music_genre: 'rock',
                movie_genre: 'Horror', status: 'active', summary: 'Yoyo')
@@ -25,7 +25,7 @@ User.delete_all
 
   User.create!(email: Faker::Internet.safe_email, password: Faker::Internet.password,
                provider: 'github', uid: 10101010, name: "user #{i}.2", bday: '1970-01-01',
-               zip: 10024, gender: gender, interested_in: interest, height: 30+i, primary_language: 'c++',
+               zip: 10024, gender: gender, interested_in: interest, height: 30+i, primary_language: 'c',
                human_language: 'English', years_programming: i, industry: 'web',
                fav_animal: 'wombat', fav_book: 'catcher', game_genre: 'RPG', music_genre: 'rock',
                movie_genre: 'Action', status: 'active', summary: 'Yoyo')
@@ -33,7 +33,7 @@ User.delete_all
 
   User.create!(email: Faker::Internet.safe_email, password: Faker::Internet.password,
                provider: 'github', uid: 10101010, name: "oldie", bday: '1950-01-01',
-               zip: 10024, gender: 'female', interested_in: 'male', height: 40, primary_language: 'c++',
+               zip: 10024, gender: 'female', interested_in: 'male', height: 40, primary_language: 'c',
                human_language: 'English', years_programming: 10, industry: 'web',
                fav_animal: 'wombat', fav_book: 'catcher', game_genre: 'Shooter', music_genre: 'rock',
                movie_genre: 'Action', status: 'active', summary: 'Yoyo')
